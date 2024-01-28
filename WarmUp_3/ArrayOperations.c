e18fee6a18af3b4f6066234e45c0ed2b0e27bcb5
Maham  M <mahamminhajuddin@gmail.com>
2024-01-28 22:14:22 +0000

    WarmUp_3 submission

---
+++ WarmUp_3/ArrayOperations.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+//intializing array and sum and average 
+int size = 5;
+int arr[5] = {1, 2, 3, 4, 5};
+int sum=0; 
+int average; 
+
+//Sum Calculation
+for (int i=0; i<size; i++) {
+    sum = sum + arr[i];
+}
+
+//Average calculation 
+average = sum/size;
+
+//Printing Calculations
+printf("Sum: %d\n", sum);
+printf("Average: %d\n", average);
+
+return 0;
+
+}
